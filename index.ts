8102723a2573552cabea84e710588564f317ca0b
Shubham Babu <shubham.babu@neosoftmail.com>
2024-10-25 20:36:56 +0530

    changes

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
-import http from "http";
 import app from "./app";
-const server = http.createServer(app);
-
 const PORT = process.env.API_PORT || 8080;
 
-server.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
